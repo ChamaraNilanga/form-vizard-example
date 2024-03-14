aa3e29fa9cbb4286940c525acfa8952329be5488
ChamaraNilanga <chamaranilanga1999@gmail.com>
2024-03-14 09:23:45 +0530

    init

---
+++ src/Component/UserForm/UserForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react';
+
+function UserForm() {
+  const [formStatus, setFormStatus] = useState(1);
+  
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default UserForm;
